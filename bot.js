7c39631bd0d659bc73fd5358ecc4fe2708d5bc39
Pythe <jwoleabu@gmail.com>
2024-05-27 22:25:41 +0100

    improved command structure by retrieving them from directory

--- bot.js
+++ bot.js
@@ -1,30 +1,89 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import {config} from "dotenv";
-import * as getip from './commands/getIp.js'
-import * as getplayerNumber from './commands/players.js'
+import { fileURLToPath } from 'url';
+import fs from 'fs'
+import path from 'path'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename)
+
 
 config();
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages],
+    intents: [GatewayIntentBits.Guilds],
     allowedMentions: { parse: [] }
 });
 
-function readyDiscord(){
-    console.log(`Booting up ${client.user.tag}`);
+client.cooldowns = new Collection();
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+        import(filePath).then(command => {
+            command = command.default
+            if ('data' in command && 'execute' in command) {
+                client.commands.set(command.data.name, command);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
+        }).catch(error => {
+            console.error(`Error importing command from ${filePath}:`, error);
+        });
+	}
 }
 
-async function handleInteraction(interaction){
-    if (!interaction.isCommand()) return;
-    if (interaction.commandName === 'serverip'){
-        await getip.execute(interaction);
-    }
-    if (interaction.commandName === 'players'){
-        await getplayerNumber.execute(interaction);
-    }
-}
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+    console.log(interaction)
+	const command = client.commands.get(interaction.commandName);
 
-client.once(Events.ClientReady, readyDiscord);
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	const { cooldowns } = interaction.client;
+
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, new Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.data.name);
+	const defaultCooldownDuration = 3;
+	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+	if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.once(Events.ClientReady, readyDiscord =>{
+    console.log(`Bot online as ${readyDiscord.user.tag}`);
+});
 
 client.login(process.env.TOKEN);
-
-client.on(Events.InteractionCreate, handleInteraction)
