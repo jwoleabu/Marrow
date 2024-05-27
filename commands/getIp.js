28f0384353ef94d5f0db66930e885c0393e20398
Pythe <jwoleabu@gmail.com>
2024-05-27 16:36:03 +0100

    linked ip to the environment variable and made the response emphemeral

--- commands/getIp.js
+++ commands/getIp.js
@@ -1,9 +1,9 @@
 import {SlashCommandBuilder} from "discord.js"
 
 export const data = new SlashCommandBuilder()
-    .setName('serverip')
+    .setName('ip')
     .setDescription('Get the ip address of the Minecraft server');
 
 export async function execute(interaction){
-    await interaction.reply('The ip is IP');
+    await interaction.reply({content:`The server ip adress is ${process.env.SERVERIP}`, ephemeral: true});
 }
